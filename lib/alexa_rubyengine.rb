8d7125a586e309871638791f1e29f88605166389
Handi Wiguna <handi@domikado.com>
2015-10-03 16:15:38 +0700

    fix require

--- lib/alexa_rubyengine.rb
+++ lib/alexa_rubyengine.rb
@@ -1,6 +1,7 @@
 require "alexa_rubyengine/version"
 require "alexa_rubyengine/verse_of_the_day_intent"
 require "alexa_rubyengine/read_single_verse_intent"
+require "alexa_rubyengine/read_verses_intent"
 require "alexa_rubyengine/random_verse_intent"
 
 module AlexaRubyengine
