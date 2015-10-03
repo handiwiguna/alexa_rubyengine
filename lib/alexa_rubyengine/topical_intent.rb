129b9cf1bcad48c3c0596b32047d817d75542898
Handi Wiguna <handi@domikado.com>
2015-10-03 20:24:47 +0700

    add verse

--- lib/alexa_rubyengine/topical_intent.rb
+++ lib/alexa_rubyengine/topical_intent.rb
@@ -9,17 +9,32 @@ class TopicalIntent
 
   TOPICS = {
     "faith" => [
-      "Romans10:17",
-      "Matthew21:22",
-      "Hebrews11:6",
-      "James2:19",
-      "Mark11:22-24",
-      "Ephesians2:8-9",
-      "Luke1:37",
-      "Hebrews11:1",
-      "Ephesians2:8"
+      { "Romans10:17" => "Romans 10 verse 17" },
+      { "Matthew21:22" => "Matthew 21 verse 22" },
+      { "Hebrews11:6" => "Hebrews 11 verse 6" },
+      { "James2:19" => "James 2 verse 19" },
+      { "Mark11:22-24" => "Mark 11 verse 22 to 24" },
+      { "Ephesians2:8-9" => "Ephesians 2 verse 8 to 9" },
+      { "Luke1:37" => "Luke 1 verse 37" },
+      { "Hebrews11:1" => "Hebrews 11 verse 1" },
+      { "Ephesians2:8" => "Ephesians 2 verse 8" }
     ]
   }
+  #
+  #
+  # TOPICS = {
+  #   "faith" => [
+  #     "Romans10:17",
+  #     "Matthew21:22",
+  #     "Hebrews11:6",
+  #     "James2:19",
+  #     "Mark11:22-24",
+  #     "Ephesians2:8-9",
+  #     "Luke1:37",
+  #     "Hebrews11:1",
+  #     "Ephesians2:8"
+  #   ]
+  # }
 
   def initialize
     @response = AlexaRubykit::Response.new
@@ -27,10 +42,12 @@ class TopicalIntent
 
   def call(request)
     topic = request.slots["topic"]["value"].downcase
-    verse = TOPICS[topic].sample
+    hash = TOPICS[topic].sample
+    key = hash.keys.first
+    verse = hash[key]
 
-    passage = RestClient.get ENDPOINT, {:params => {:passage => verse , 'key' => 'fd37d8f28e95d3be8cb4fbc37e15e18e'}}
-    @response.add_speech(passage)
+    passage = RestClient.get ENDPOINT, {:params => {:passage => key , 'key' => 'fd37d8f28e95d3be8cb4fbc37e15e18e'}}
+    @response.add_speech("%s. %s" % [verse, passage])
     @response
   end
 end
