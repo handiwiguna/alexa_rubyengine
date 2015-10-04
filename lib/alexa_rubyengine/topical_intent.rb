bc305553d99a6908ea5f71cf80fdee241f49f600
Handi Wiguna <handi@domikado.com>
2015-10-04 08:11:47 +0700

    add utf-8 header

--- lib/alexa_rubyengine/topical_intent.rb
+++ lib/alexa_rubyengine/topical_intent.rb
@@ -9,20 +9,6 @@ class TopicalIntent
 
   ENDPOINT = "http://api.biblia.com/v1/bible/content/asv.txt.txt"
 
-  # TOPICS = {
-  #   "faith" => [
-  #     { "Romans10:17" => "Romans 10 verse 17" },
-  #     { "Matthew21:22" => "Matthew 21 verse 22" },
-  #     { "Hebrews11:6" => "Hebrews 11 verse 6" },
-  #     { "James2:19" => "James 2 verse 19" },
-  #     { "Mark11:22-24" => "Mark 11 verse 22 to 24" },
-  #     { "Ephesians2:8-9" => "Ephesians 2 verse 8 to 9" },
-  #     { "Luke1:37" => "Luke 1 verse 37" },
-  #     { "Hebrews11:1" => "Hebrews 11 verse 1" },
-  #     { "Ephesians2:8" => "Ephesians 2 verse 8" }
-  #   ]
-  # }
-
   TOPICS = {
     "faith" => [
       { "Romans10:17" => "Romans 10 verse 17" },
@@ -46,22 +32,6 @@ class TopicalIntent
       { "Colossians3:5" => "Colossians 3 verse 5" },
     ]
   }
-
-  #
-  #
-  # TOPICS = {
-  #   "faith" => [
-  #     "Romans10:17",
-  #     "Matthew21:22",
-  #     "Hebrews11:6",
-  #     "James2:19",
-  #     "Mark11:22-24",
-  #     "Ephesians2:8-9",
-  #     "Luke1:37",
-  #     "Hebrews11:1",
-  #     "Ephesians2:8"
-  #   ]
-  # }
 
   def initialize
     @response = AlexaRubykit::Response.new
